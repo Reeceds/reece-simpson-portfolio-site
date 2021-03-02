b2f447822f60d968759bd320cd22851cd52ef913
Reece Simpson <reece@reeces-mbp.home>
2021-03-02 14:22:09 +0000

    Added view more feature and added projects to the proj section

--- js/index.js
+++ js/index.js
@@ -32,39 +32,20 @@ ScrollReveal().reveal(".slide-up-one", slideUp(0));
 ScrollReveal().reveal(".slide-up-two", slideUp(250));
 ScrollReveal().reveal(".slide-up-three", slideUp(500));
 ScrollReveal().reveal(".slide-up-four", slideUp(750));
-ScrollReveal().reveal(".slide-up-five", slideUp(1000));
-ScrollReveal().reveal(".slide-up-six", slideUp(1250));
-ScrollReveal().reveal(".slide-up-seven", slideUp(1500));
 
-// let name = "Print this sentence";
-
-// for (let i = 0; i < name.length; i++) {
-//   task(i);
-// }
-
-// function task(i) {
-//   setTimeout(function () {
-//     console.log(name[i]);
-//   }, 500 * i);
-// }
-
-// const ex = (num) => {
-//   return new Promise((resolve, reject) => {
-//     if (ex > 4) {
-//       resolve(`${num} is more than 4`);
-//     } else {
-//       reject(`${num} is less than 4`);
-//     }
-//   });
-// };
-
-// const prom = async (boi) => {
-//   try {
-//     const response = await ex(boi);
-//     console.log(response);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// prom(8);
+// Skills section view more ///////////////////////////
+jQuery(document).ready(function () {
+  // If more than 4  skill-collection-container, hide the remaining
+  $(".skill-collection-container .skill-individual-container")
+    .slice(0, 4)
+    .addClass("shown");
+  $(".skill-collection-container .skill-individual-container")
+    .not(".shown")
+    .hide();
+  $(".showMore").on("click", function () {
+    $(".skill-collection-container .skill-individual-container")
+      .not(".shown")
+      .toggle(300);
+    $(this).toggleClass("showLess");
+  });
+});
