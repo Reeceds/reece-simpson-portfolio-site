186cfc03e1b29bfec90415f1b1fc9fb2718831b8
Reeceds <32424143+Reeceds@users.noreply.github.com>
2022-09-06 02:30:28 -0400

    Add email button slide animation

--- js/index.js
+++ js/index.js
@@ -1,33 +1,37 @@
 // Skills dropdown section
 $(".skill-individual-container").each(function (i, skill) {
-  if (i > 3) {
-    $(skill).addClass("hide-skill");
-  }
+    if (i > 3) {
+        $(skill).addClass("hide-skill");
+    }
 });
 
 $(".view-more-btn").on("click", function () {
-  $(".skill-individual-container").each(function (i, skill) {
-    if (i > 3) {
-      $(this).toggleClass("hide-skill");
-    }
-  });
+    $(".skill-individual-container").each(function (i, skill) {
+        if (i > 3) {
+            $(this).toggleClass("hide-skill");
+        }
+    });
 
-  if ($(".view-more-btn").html() === "View more") {
-    $(".view-more-btn").html("View less");
-  } else {
-    $(".view-more-btn").html("View more");
-  }
+    if ($(".view-more-btn").html() === "View more") {
+        $(".view-more-btn").html("View less");
+    } else {
+        $(".view-more-btn").html("View more");
+    }
 });
 
 // Click hero button to scroll
 function scrollToSection(section) {
-  $("html,body").animate({ scrollTop: $(section).offset().top });
+    $("html,body").animate({ scrollTop: $(section).offset().top });
 }
 
 $(".js-hero-about-me-btn").click(function () {
-  scrollToSection("#about-me");
+    scrollToSection("#about-me");
 });
 
 $(".js-hero-projects-btn").click(function () {
-  scrollToSection("#projects");
+    scrollToSection("#projects");
+});
+
+$(".js-email-icon").click(function () {
+    scrollToSection("#message-me");
 });
